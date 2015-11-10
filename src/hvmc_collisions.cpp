722c2cdd8cb9c1da74c80cadd3a1bcf96150408e
Sephilys <mlop-65@hotmail.fr>
2015-11-10 18:02:00 +0100

    collision des sphere mais on a perdu les versions précédentes et ça marche plus

--- src/hvmc_collisions.cpp
+++ src/hvmc_collisions.cpp
@@ -1,2 +1,53 @@
 #include "hvmc_collisions.h"
 #include "hvmc_physics.h"
+
+
+
+/**
+* petit calcul alakon
+**/
+
+double norm(vec2 vect){
+    return sqrt (vect.x * vect.x + vect.y * vect.y);
+}
+
+
+/**
+ *out : CollisionInfo info
+ **/
+bool CollideSphereSphere( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info){
+
+    double dist = sqrt ((rb1->position.x -rb2->position.x) * (rb1->position.x -rb2->position.x) + (rb1->position.y -rb2->position.y) * (rb1->position.y -rb2->position.y) );
+
+    if (dist < rb1->collider.radius +  rb2->collider.radius ){
+
+        info.contactNormal = Normalize(rb2->position - rb1->position);
+        info.interPenetrationDistance = rb2->collider.radius + rb1->collider.radius - norm(rb2->position - rb1->position);
+        info.contactPosition = rb1->position + rb1->collider.radius * info.contactNormal;
+        info.rb1 = rb1;
+        info.rb2 = rb2;
+        return true;
+    }
+    return false;
+}
+
+
+
+/**
+ *out : CollisionInfo info
+ **/
+ /*
+bool Collide( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info ){
+
+    if (( rb1->collider.type == RIGID_BODY_SPHERE) && (rb2->collider.type == RIGID_BODY_SPHERE)){
+
+        if (CollideCircleCircle(rb1, rb2, info)){
+            info.rb1 = rb1;
+            info.rb2 = rb2;
+            return true;
+        }
+        return false;
+   }
+    return false;
+
+}*/
--- src/hvmc_collisions.h
+++ src/hvmc_collisions.h
@@ -3,6 +3,7 @@
 
 #include "hvmc_math.h"
 
+
 struct RigidBody;
 
 struct CollisionInfo{
@@ -13,18 +14,21 @@ struct CollisionInfo{
     double interPenetrationDistance;    //distance interpenetration
     vec2 contactPosition;   //position du point (ses coordonnées en fait) au contact tu vois
 
+    CollisionInfo() {}
 
-    CollisionInfo(RigidBody* rb1, RigidBody* rb2){
+    CollisionInfo(RigidBody* rb1, RigidBody* rb2, vec2 contactNormal, double penetration, vec2 contactPoint){
         this->rb1 = rb1;
         this->rb2 = rb2;
+        this->contactNormal = contactNormal;
+        this->interPenetrationDistance = penetration;
+        this->contactPosition = contactPoint;
     }
-
-
 
 
 
 };
 
+bool CollideSphereSphere( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info);
 
 
 
