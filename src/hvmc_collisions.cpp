9b7b44507da65e69b633e9a9987e1c19984798b6
Sephilys <mlop-65@hotmail.fr>
2015-12-07 13:43:20 +0100

    full merge qui bug sans mur popopo

--- src/hvmc_collisions.cpp
+++ src/hvmc_collisions.cpp
@@ -4,7 +4,7 @@
 
 
 /**
-* petit calcul alakon
+* return : norme du vecteur
 **/
 
 double norm(vec2 vect){
@@ -54,11 +54,11 @@ bool CollideBoxBox( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info){
 
     /*test d'un overlap en abscisse*/
 
-    if ((rb1XMin > rb2XMin) && (rb1XMin<rb2XMax)){
+    if ((rb1XMin >= rb2XMin) && (rb1XMin <= rb2XMax)){
         overlapX = abs(rb1XMin - rb2XMax);
     }
 
-    else if ((rb2XMin > rb1XMin) && (rb2XMin<rb1XMax)){
+    else if ((rb2XMin >= rb1XMin) && (rb2XMin <= rb1XMax)){
         overlapX = abs(rb2XMin - rb1XMax);
     }
 
@@ -67,12 +67,12 @@ bool CollideBoxBox( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info){
 
     /*test d'un overlap en ordonnée*/
 
-    if ((rb1YMin > rb2YMin) && (rb1YMin<rb2YMax)){
+    if ((rb1YMin >= rb2YMin) && (rb1YMin <= rb2YMax)){
         overlapY = abs(rb1YMin - rb2YMax);
         rb1sup = true;
     }
 
-    else if ((rb2YMin > rb1YMin) && (rb2YMin<rb1YMax)){
+    else if ((rb2YMin >= rb1YMin) && (rb2YMin <= rb1YMax)){
         overlapY = abs(rb2YMin - rb1YMax);
         rb1sup = false;
     }
@@ -107,74 +107,223 @@ bool CollideBoxBox( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info){
 
 
     /*provisoire*/
-    info.contactPosition = rb1->position + rb1->collider.dims.x * info.contactNormal;
+    //info.contactPosition = rb1->position + rb1->collider.dims.x * info.contactNormal;
 
     return true;
 
 }
 
 
+
+bool CollideCircleBox( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info){
+
+    double overlapX, overlapY;
+
+    double rb2XMin = rb2->position.x;
+    double rb2XMax = rb2->position.x +rb2->collider.dims.x;
+    double rb2YMin = rb2->position.y;
+    double rb2YMax = rb2->position.y +rb2->collider.dims.y;
+
+    double rb1XMin = rb1->position.x-rb1->collider.radius;
+    double rb1XMax = rb1->position.x+rb1->collider.radius;
+    double rb1YMin = rb1->position.y-rb1->collider.radius;
+    double rb1YMax = rb1->position.y +rb1->collider.radius;
+
+    bool rb1sup;//rb1 au dessus de rb2
+
+    /*test d'un overlap en abscisse*/
+
+    if ((rb1XMin >= rb2XMin) && (rb1XMin<=rb2XMax)){
+        overlapX = abs(rb1XMin - rb2XMax);
+    }
+
+    else if ((rb2XMin >= rb1XMin) && (rb2XMin<=rb1XMax)){
+        overlapX = abs(rb2XMin - rb1XMax);
+    }
+
+    else return false;
+
+
+    /*test d'un overlap en ordonnée*/
+
+    if ((rb1YMin >= rb2YMin) && (rb1YMin<=rb2YMax)){
+        overlapY = abs(rb1YMin - rb2YMax);
+        rb1sup = true;
+    }
+
+    else if ((rb2YMin >= rb1YMin) && (rb2YMin<=rb1YMax)){
+        overlapY = abs(rb2YMin - rb1YMax);
+        rb1sup = false;
+    }
+
+    else return false;
+
+    /// à partir de ici il y a bien collision
+
+
+
+
+    if (overlapX < overlapY){
+
+        info.interPenetrationDistance = overlapX;
+
+        if (rb1sup)
+            info.contactNormal = vec2{-1,0};
+        else
+            info.contactNormal = vec2{1,0};
+    }else{
+
+        info.interPenetrationDistance = overlapY;
+
+        if (rb1sup)
+            info.contactNormal = vec2{0,-1};
+        else
+            info.contactNormal = vec2{0,1};
+    }
+
+    info.rb1 = rb1;
+    info.rb2 = rb2;
+
+
+
+    //info.contactPosition = rb1->position + rb1->collider.radius * info.contactNormal;
+
+    return true;
+
+}
+
+
+
+bool CollideBoxCircle( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info){
+
+    double overlapX, overlapY;
+
+    double rb1XMin = rb1->position.x;
+    double rb1XMax = rb1->position.x +rb1->collider.dims.x;
+    double rb1YMin = rb1->position.y;
+    double rb1YMax = rb1->position.y +rb1->collider.dims.y;
+
+    double rb2XMin = rb2->position.x-rb2->collider.radius;
+    double rb2XMax = rb2->position.x+rb2->collider.radius;
+    double rb2YMin = rb2->position.y-rb2->collider.radius;
+    double rb2YMax = rb2->position.y +rb2->collider.radius;
+
+    bool rb1sup;//rb1 au dessus de rb2
+
+    /*test d'un overlap en abscisse*/
+
+    if ((rb1XMin >= rb2XMin) && (rb1XMin<=rb2XMax)){
+        overlapX = abs(rb1XMin - rb2XMax);
+    }
+
+    else if ((rb2XMin >= rb1XMin) && (rb2XMin<=rb1XMax)){
+        overlapX = abs(rb2XMin - rb1XMax);
+    }
+
+    else return false;
+
+
+    /*test d'un overlap en ordonnée*/
+
+    if ((rb1YMin >= rb2YMin) && (rb1YMin<=rb2YMax)){
+        overlapY = abs(rb1YMin - rb2YMax);
+        rb1sup = true;
+    }
+
+    else if ((rb2YMin >= rb1YMin) && (rb2YMin<=rb1YMax)){
+        overlapY = abs(rb2YMin - rb1YMax);
+        rb1sup = false;
+    }
+
+    else return false;
+
+    /// à partir de ici il y a bien collision
+
+
+
+
+    if (overlapX < overlapY){
+
+        info.interPenetrationDistance = overlapX;
+
+        if (rb1sup)
+            info.contactNormal = vec2{-1,0};
+        else
+            info.contactNormal = vec2{1,0};
+    }else{
+
+        info.interPenetrationDistance = overlapY;
+
+        if (rb1sup)
+            info.contactNormal = vec2{0,-1};
+        else
+            info.contactNormal = vec2{0,1};
+    }
+
+    info.rb1 = rb1;
+    info.rb2 = rb2;
+
+
+
+    //info.contactPosition = rb1->position + rb1->collider.radius * info.contactNormal;
+
+    return true;
+
+}
+
 void CollisionInfo::Solve() const
 {
-//    rb1->ApplyForce(vec2{10, 0});
-//    float m_a = rb1->m;
-//    float m_b = rb2->m;
-//    vec2 v_a = rb1->velocity;
-//    vec2 v_b = rb2->velocity;
+    // Renommage des données
+//    f32 m_a = rb1->m;
+//    f32 m_b = rb2->m;
+    f32 im_a = rb1->im;
+    f32 im_b = rb2->im;
+    f32 iI_a = rb1->iI;
+    f32 iI_b = rb2->iI;
+    vec2 v_a = rb1->velocity;
+    vec2 v_b = rb2->velocity;
+    f32 w_a = rb1->angularVelocity;
+    f32 w_b = rb2->angularVelocity;
 
-//    float e = -(m_a * a_a - m_b * a_b) / (m_a * v_a - m_b * v_b);
-//    float e = 1.f;
-//    float J = (-(1 + e) * Dot(v_rel, n)) * ;
-    rb1->ApplyImpulse(-10 * contactNormal, vec2{0, 0});
-//    rb2->ApplyImpulse();
+    vec2 n = contactNormal; //
+
+    vec2 r_a =  rb1->collider.radius * n; //
+    vec2 r_b = -rb2->collider.radius * n; //
+    vec2 v_rel = (v_b + Cross(w_b, r_b)) - (v_a + Cross(w_a, r_a)); //
+
+//    f32 e = -(m_a * a_a - m_b * a_b) / (m_a * v_a - m_b * v_b);
+    f32 e = 1.f;
+
+    f32 J = (-(1 + e) * Dot(v_rel, n)) / (im_a + im_b + iI_a * Cross(r_a, n) + iI_b * Cross(r_b, n)); //
+
+    vec2 j_a = -J * n;
+    vec2 j_b =  J * n;
+
+    rb1->ApplyImpulse(j_a, vec2{0, 0});
+    rb2->ApplyImpulse(j_b, vec2{0, 0});
 }
+
 
 
 /**
  *out : CollisionInfo info
  **/
-/*
-bool Collide( RigidBody* rb1, RigidBody* rb2, CollisionInfo &info ){
-
-
-    if (( rb1->collider.type == RIGID_BODY_SPHERE) && (rb2->collider.type == RIGID_BODY_SPHERE)){
-
-        if (CollideCircleCircle(rb1, rb2, info)) {
-            std::cout<<"yop"<<std::endl;
-            return true;
-        }
-        return false;
-    }
-    else if (( rb1->collider.type == RIGID_BODY_BOX) && (rb2->collider.type == RIGID_BODY_BOX)){
-        std::cout<<"coucou"<<std::endl;
-        if (CollideBoxBox(rb1, rb2, info)){
-            std::cout<<"ça touche"<<std::endl;
-            return true;
-        }
-        return false;
-    }
-    return false;
-
-}
-
-*/
-
 bool Collide( RigidBody* a, RigidBody* b, CollisionInfo& info ) {
 
     if ( a->collider.type == RIGID_BODY_BOX )
     {
         if ( b->collider.type == RIGID_BODY_BOX )
             return CollideBoxBox( a, b, info );
-        //else if ( b->collider.type == RIGID_BODY_SPHERE )
-          //  return CollideBoxSphere( a, b, info );
+        else if ( b->collider.type == RIGID_BODY_SPHERE )
+            return CollideBoxCircle( a, b, info );
     }
 
     else if ( a->collider.type == RIGID_BODY_SPHERE )
     {
         if ( b->collider.type == RIGID_BODY_SPHERE )
             return CollideCircleCircle( a, b, info );
-        //else if ( b->collider.type == RIGID_BODY_BOX )
-          //  return CollideSphereBox( a, b, info );
+        else if ( b->collider.type == RIGID_BODY_BOX )
+            return CollideCircleBox( a, b, info );
     }
 
     // Should not get there
